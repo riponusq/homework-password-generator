5c344be27cccd487ed43de46705f03e4f7b102f0
Mohammad Chowdhury <riponusq@yahoo.com>
2021-09-09 23:33:29 +1000

    readme updated

---
+++ Develop/script.js
@@ -0,0 +1,65 @@
+var generateBtn = document.querySelector("#generate");
+
+//var for all possible password
+
+var lowercaseChar = "abcdefghijklmnopqrstuvwxyz";
+var uppercaseChar = "ABCDEFGHIJKLMNOPQRSTUZWXYZ";
+var numericalChar = "0123456789";
+var specialChar = "!#$%&'()*+-./:;<=>?@[\^_`{|}~";
+
+
+  function generatePassword() {
+    var password = "";
+    var passwordChar = "";
+
+    var passwordLengthUser = prompt("Password must be between 8-128 characters.");
+    passwordLengthUser = parseInt(passwordLengthUser);
+
+    if (passwordLengthUser < 8) {
+    alert("Password must have more than 7 characters!");
+    return "";
+    }
+    if (passwordLengthUser > 128) {
+    alert("Password no more than 128 characters!");
+    return ""
+    }  
+    
+    var lowercaseCharactersChoice = confirm("To make it secure put some lowercase characters?");
+    if (lowercaseCharactersChoice) {
+    passwordChar += lowercaseChar;
+    }
+
+    var uppercaseCharactersChoice = confirm("for secure some uppercase characters?");
+    if (uppercaseCharactersChoice) {
+    passwordChar += uppercaseChar;
+    }
+
+    var numericalCharactersChoice = confirm("and also some numerical characters");
+    if (numericalCharactersChoice) {
+    passwordChar += numericalChar;
+    }
+
+    var specialCharacterChoice = confirm(" To make it more secure put some symbols");
+    if (specialCharacterChoice) {
+    passwordChar += specialChar;
+    }
+
+
+    for (var i = 0; i < passwordLengthUser; i++) {
+    password =password+ passwordChar[Math.floor(Math.random() * passwordChar.length)]
+    }
+  
+  return password;
+  }
+
+  function writePassword() {
+    
+    password = generatePassword();
+    console.log(password);
+  var passwordText = document.querySelector("#password");
+  
+    passwordText.value = password
+  
+  }
+
+  generateBtn.addEventListener("click", writePassword);
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# homework-password-generator
+ ##password-generator-homework
+
+This is the homework of Password Generator using javaScript . I tried to make it as good as I can.
+
+I have made this homework in the following ways:
+I used variable for all posible characters and symbol and special character.
+Creates user prompt to select password length.
+Creates confirm for lowercase.
+creates confirm for uppercase.
+Creates confirm for special characters.
+Creates confirm for symbols.
+When all prompts are answered a password generated that matches selected criteria.
+And also the password is displayed in generated area.
+
+## Credits:
+
+Ben Atkinson. (Instructor)
+Yun Zhao. (TA)
+W3 schoolwebsite.
